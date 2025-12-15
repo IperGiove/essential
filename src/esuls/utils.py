90bae8b1c1b9f56112e3eb9efef86f0419f9a49c
ipergiove <ipergiove@gmail.com>
2025-12-15 23:54:30 +0900

    accept coroutine

--- src/esuls/utils.py
+++ src/esuls/utils.py
@@ -8,22 +8,21 @@ T = TypeVar("T")
 
 
 async def run_parallel(
-    *functions: Callable[[], Awaitable[T]],
+    *coroutines: Awaitable[T],
     limit: int = 20
 ) -> List[T]:
-    """
-    run parallel multiple functions
-    """
+    """Run parallel coroutines with semaphore limit"""
+    
     semaphore = asyncio.Semaphore(limit)
-
-    async def limited_function(func: Callable[[], Awaitable[T]]) -> T:
+    
+    async def limited_coroutine(coro: Awaitable[T]) -> T:
         async with semaphore:
-            return await func()
-
-    tasks = [asyncio.create_task(limited_function(func)) for func in functions]
-
+            return await coro
+    
+    tasks = [asyncio.create_task(limited_coroutine(coro)) for coro in coroutines]
+    
     results = []
     for fut in asyncio.as_completed(tasks):
         results.append(await fut)
-
+    
     return results
